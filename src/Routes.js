6506dca20fc04668891153a73ff03ba5ca48c724
kiran-mnc <developer9@mynewcar.in>
2020-05-10 00:56:28 +0530

    code splitting via router

--- src/Routes.js
+++ src/Routes.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './Components/Home';
-import FourNotFour from './Components/FourNotFour';
-import Recipe from './Components/Recipe';
-import Resources from './Components/Resources';
+
+const Home = lazy(() => import('./Components/Home'));
+const FourNotFour = lazy(() => import('./Components/FourNotFour'));
+const Recipe = lazy(() => import('./Components/Recipe'));
+const Resources = lazy(() => import('./Components/Resources'));
 
 const Routes = () => (
   <Router>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/recipe/:categoryId" component={Recipe} />
-      <Route path="/resources" component={Resources} />
-      <Route path="*" component={FourNotFour} />
-    </Switch>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/recipe/:categoryId" component={Recipe} />
+        <Route path="/resources" component={Resources} />
+        <Route path="*" component={FourNotFour} />
+      </Switch>
+    </Suspense>
   </Router>
 );
 
