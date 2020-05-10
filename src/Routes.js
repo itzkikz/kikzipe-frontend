4bd312ab1097e7b165c4681242ac60fb84587065
kiran-mnc <developer9@mynewcar.in>
2020-05-10 11:59:29 +0530

    performance optimization

--- src/Routes.js
+++ src/Routes.js
@@ -1,22 +1,24 @@
-import React, { Suspense, lazy } from 'react';
+import React from 'react';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Components/Home';
+import FourNotFour from './Components/FourNotFour';
+import Recipe from './Components/Recipe';
+import Resources from './Components/Resources';
 
-const Home = lazy(() => import('./Components/Home'));
-const FourNotFour = lazy(() => import('./Components/FourNotFour'));
-const Recipe = lazy(() => import('./Components/Recipe'));
-const Resources = lazy(() => import('./Components/Resources'));
+// const Home = lazy(() => import('./Components/Home'));
+// const FourNotFour = lazy(() => import('./Components/FourNotFour'));
+// const Recipe = lazy(() => import('./Components/Recipe'));
+// const Resources = lazy(() => import('./Components/Resources'));
 
 const Routes = () => (
   <Router>
-    <Suspense fallback={<div>Loading...</div>}>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/recipe/:categoryId" component={Recipe} />
-        <Route path="/resources" component={Resources} />
-        <Route path="*" component={FourNotFour} />
-      </Switch>
-    </Suspense>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/recipe/:categoryId" component={Recipe} />
+      <Route path="/resources" component={Resources} />
+      <Route path="*" component={FourNotFour} />
+    </Switch>
   </Router>
 );
 
